35f4320fb2cad13a34724b56b5271681664442ef
Madhav <madhavkhanna2021@u.northwestern.edu>
2020-10-24 13:24:01 -0500

    Added CustomShader.compute

---
+++ Assets/Scripts/Density/CustomDensity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomDensity : DensityGenerator
+{
+    public override ComputeBuffer Generate(ComputeBuffer pointsBuffer, int numPointsPerAxis, float boundsSize, Vector3 worldBounds, Vector3 centre, Vector3 offset, float spacing)
+    {
+        return base.Generate(pointsBuffer, numPointsPerAxis, boundsSize, worldBounds, centre, offset, spacing);
+    }
+}
